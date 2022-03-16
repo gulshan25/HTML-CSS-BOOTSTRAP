85f2f19f5bce2699babe0c6607eabdabb80169d0
gulshan25 <g.r.tanny@gmail.com>
2022-03-16 14:45:57 +0600

    practice

---
+++ learnWithSumit/test.js
@@ -0,0 +1,23 @@
+function sleep (name, time) {
+    console.log (name + ' is sleeping from ' + time);
+    return 5;
+}
+
+//console.log(name);
+sleep('rahim', '11 pm');
+//sleep('karim', '11 pm');  
+//sleep('jasim', '11 pm');
+
+console.log(4);
+
+function myFunction(a, b) {
+//    console.log("hello");
+}
+let x = myFunction(4, 3);
+
+
+let x = myFunction(4, 3);
+
+function myFunction(a, b) {
+    return a * b;
+}
